db502ec906109a4448441ff55ecfb67ea4d099d9
Joon Ho <Joon Ho>
2016-02-27 23:53:14 -0800

    init android work

---
+++ FriendlyAndroid/app/src/main/java/com/friendlyandroid/friendlyandroid/MainActivity.java
@@ -0,0 +1,66 @@
+package com.friendlyandroid.friendlyandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bLogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bLogout = (Button) findViewById(R.id.bLogout);
+
+        bLogout.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.bLogout:
+                Intent loginIntent = new Intent(this, Login.class);
+                startActivity(loginIntent);
+                break;
+        }
+    }
+}
---
+++ FriendlyAndroid/app/src/main/java/com/friendlyandroid/friendlyandroid/Register.java
@@ -0,0 +1,80 @@
+package com.friendlyandroid.friendlyandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.IOException;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    Button bRegister;
+    EditText etFirstName, etLastName, etUsername, etPassword;
+    TextView loginLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etFirstName = (EditText) findViewById(R.id.etFirstName);
+        etLastName = (EditText) findViewById(R.id.etLastName);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bRegister = (Button) findViewById(R.id.bRegister);
+        loginLink = (TextView) findViewById(R.id.tvLoginLink);
+
+        bRegister.setOnClickListener(this);
+        loginLink.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.bRegister:
+                break;
+            case R.id.tvLoginLink:
+                Intent registerIntent = new Intent(Register.this, Login.class);
+                startActivity(registerIntent);
+                break;
+        }
+    }
+
+    public void sendRegister() {
+        final TextView mTextView = (TextView) findViewById(R.id.text);
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
